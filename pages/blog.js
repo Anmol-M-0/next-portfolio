89ea0210fe69b508c6d6120424b0b1db4ef94278
Anmol-M-0 <amaniyar000@gmail.com>
2021-05-20 08:15:02 +0530

    some changes

--- pages/blog.js
+++ pages/blog.js
@@ -1,9 +1,9 @@
 import Layout from "../components/Layout"
 import Link from "next/link";
 
-const PostLink = ({title})=>(
+const PostLink = ({title, slug})=>(
     <li>
-        <Link href={`/post?title=${title}`}>
+        <Link as={`/${slug}`} href={`/post?title=${title}`}>
             <a>{title} post</a>
         </Link>
     </li>
@@ -12,9 +12,9 @@ const Blog = () => {
     return (
         <Layout title="myblog">
             <ul>
-                <PostLink title='React'/>
-                <PostLink title='Blog'/>
-                <PostLink title='Wiki'/>
+                <PostLink slug='React-post' title='React'/>
+                <PostLink slug='Blog-post' title='Blog'/>
+                <PostLink slug='Wiki-post' title='Wiki'/>
             </ul>
         </Layout>
     )
